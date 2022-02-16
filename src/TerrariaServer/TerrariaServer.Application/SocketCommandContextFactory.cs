b52d7486bc1408614af5b77921f3f175a2ca3a07
MeuLee <mathieularivee00@gmail.com>
2022-02-15 21:02:15 -0500

    separate class with interfaces into multiple partial classes

--- src/TerrariaServer/TerrariaServer.Application/SocketCommandContextFactory.cs
+++ src/TerrariaServer/TerrariaServer.Application/SocketCommandContextFactory.cs
@@ -14,21 +14,27 @@ public interface ISocketCommandContextProvider
 	SocketCommandContext ProvideContext(ulong messageId);
 }
 
-internal class SocketCommandContextFactory : ISocketCommandContextFactory, ISocketCommandContextProvider
+internal partial class SocketCommandContextFactory : ISocketCommandContextProvider
 {
 	private readonly TimeSpan _cacheTimeout = TimeSpan.FromMinutes(1);
 	private readonly IMemoryCache _memoryCache;
 
 	public SocketCommandContextFactory(IMemoryCache memoryCache)
 		=> _memoryCache = memoryCache;
+}
 
+internal partial class SocketCommandContextFactory : ISocketCommandContextFactory
+{
 	public SocketCommandContext CreateContext(DiscordSocketClient client, SocketUserMessage message)
 	{
 		var context = new SocketCommandContext(client, message);
 		_memoryCache.Set(message.Id, context, _cacheTimeout);
 		return context;
 	}
+}
 
+internal partial class SocketCommandContextFactory : ISocketCommandContextProvider
+{
 	public SocketCommandContext ProvideContext(ulong messageId)
 		=> _memoryCache.Get<SocketCommandContext>(messageId);
 }
